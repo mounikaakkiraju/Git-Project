db1a6cccfd6d4ee75425714b9c8a372c1fa75582
mouni <mouni@mounika>
2024-04-20 12:34:15 +0530

    export to git

---
+++ GIT/src/test/java/NEW/Class1Test.java
@@ -0,0 +1,17 @@
+package NEW;
+
+import org.testng.annotations.Test;
+@Test
+public class Class1Test
+{
+	
+	public void method1()
+	{
+		System.out.println("hi");
+	}
+	public void method2()
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ GIT/src/test/java/NEW/Class2Test.java
@@ -0,0 +1,18 @@
+package NEW;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Class2Test 
+{
+
+	public void method1()
+	{
+		System.out.println("good");
+	}
+	public void method2()
+	{
+		System.out.println("morning");
+	}
+
+}
